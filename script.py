3e77c5f2ed91f13b2f852b98887056d86e7ecd77
Daniel Fernandes Costa <pr.danfc88@gmail.com>
2024-07-03 23:30:50 -0300

    Mudanças nos Gráficos

--- script.py
+++ script.py
@@ -1,3 +1,4 @@
+# Importar as bibliotecas necessárias
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
@@ -27,15 +28,36 @@ print(f"\nCurso com o maior número de vendas: {curso_mais_vendido['Nome do Curs
 # Visualizar a distribuição das vendas ao longo do tempo
 df['Data'] = pd.to_datetime(df['Data'])
 
+
 # Gráfico de barras para contagem de categorias
 plt.figure(figsize=(12, 6))
-sns.barplot(x='Nome do Curso', y='Quantidade de Vendas', data=df)
-plt.xticks(rotation=90)
+ax = sns.barplot(x='Nome do Curso', y='Quantidade de Vendas', data=df)
+plt.xticks(rotation=45, ha='right', fontsize=8) 
 plt.title('Quantidade de Vendas por Curso')
+plt.xlabel('Nome do Curso')
+plt.ylabel('Quantidade de Vendas')
+plt.tight_layout()
+
+# Adicionar rótulos de dados (labels) no gráfico de barras
+for p in ax.patches:
+    ax.annotate(f"{p.get_height():.0f}", (p.get_x() + p.get_width() / 2., p.get_height()), ha='center', va='bottom', fontsize=12, color='black', xytext=(0, 5), textcoords='offset points')
+
 plt.show()
+
+sns.set(style='whitegrid', rc={'patch.edgecolor': 'none'})
 
 # Gráfico de dispersão para relação entre variáveis
 plt.figure(figsize=(12, 6))
-sns.scatterplot(x='Data', y='Quantidade de Vendas', size='Receita', data=df, legend=False)
+sns.scatterplot(x='Data', y='Quantidade de Vendas', size='Receita', data=df, legend=False, sizes=(20, 200))
 plt.title('Quantidade de Vendas ao Longo do Tempo')
+plt.xlabel('Data')
+plt.ylabel('Quantidade de Vendas')
+plt.tight_layout()
 plt.show()
+
+# Adicionar rótulos de dados (labels) no gráfico de dispersão
+for line in range(0, df.shape[0]):
+    ax.text(df['Data'].iloc[line], df['Quantidade de Vendas'].iloc[line], df['Nome do Curso'].iloc[line], horizontalalignment='right', size='medium', color='black', weight='semibold')
+
+plt.show()
+
